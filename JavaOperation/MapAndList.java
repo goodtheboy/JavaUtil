3ecde10c4ecd538aafa4811221b7dd689130a87e
goodtheboy <ferdiep@live.cn>
2017-12-08 11:56:54 +0800

    数据交集和反转
    基于List和Map的特性进行简单封装

--- JavaOperation/MapAndList.java
+++ JavaOperation/MapAndList.java
@@ -2,100 +2,65 @@ package com.sunsd.lfw.modules.agency.entity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Iterator;
 
 public class MapAndList {
-	/**
-	  * 使用Map和List的特性进行匹配:
-	  * Map为key-value结构，不能放重复数据
-	  * List可以放重复数据
-	  * 使用String型id做key,List<Person>做value
-	  * 遍历List<String>, map.get(String)则取出id == str 的List
-	  */
-	public static void main(String[] args) {
-		MapAndList();
-	}
 	
-	
-	 private static void  MapAndList(){
-	  //构造一个10万个Person对象的list
-	  List<Person> personList = new ArrayList<Person>();
-	  
-	  for (int i = 0; i < 100000; i++) {
-	   int j =10000;
-	   personList.add(new Person((i%j) +"", "张三"+i));
-	  }
-	  //构造一个1万个String对象的list
-	  List<String> strList = new ArrayList<String>();
-	  for (int i = 0; i < 10000; i++) {
-	   strList.add(i +"");
-	  }
-	  
-	  long start = System.currentTimeMillis();
-	  //利用Map和List的特性整理数据
-	        Map<String, List<Person>> map = new HashMap<String, List<Person>>();
-	        //将10W条数据根据id放入map
-	        for(int i=0;i<personList.size();i++){
-	         Person person = personList.get(i);
-	             String id = person.getId();
-	              if(map.containsKey(id)){
-	                  map.get(id).add(person);
-	              }else{
-	                   List<Person> pList = new ArrayList<Person>();
-	                   pList.add(person);
-	                   //id为key,相同id的person的List为value
-	                   map.put(id,pList);
-	             }
-	        }
-	      //保存匹配结果
-	        List<Person> resultList = new ArrayList<Person>();
-	        //根据1W条str,map.get(str)取匹配上的数据
-	        for (String str : strList) {
-	            List<Person> pList = map.get(str);
-	            if (pList != null) {
-	                resultList.addAll(pList);
-	            }
-	        }
-	        long end = System.currentTimeMillis();
-	        System.out.println("map.size:" +map.size());
-	        System.out.println("reslutList.size:"+ resultList.size());
-	        System.out.println("10W+1W次循环耗时："+ (end - start) + "毫秒");
-	        System.out.println("----------------------------");
-	 }
-
-	}
-
 	/**
-	 * Person实体类
+	 * 获取HashMap中交集/反转于ArrayList的key值,inversion做控制参数
+	 * @param al
+	 * @param hm
+	 * @param inversion
+	 * @return
 	 */
-	class Person{
-	 private String id;
-	 private String name;
-
-	 public Person() {}
-	 
-	 public Person(String id, String name) {
-	  this.id = id;
-	  this.name = name;
-	 }
-
-	 @Override
-	 public String toString() {
-	  return this.id +"::>"+ this.name;
-	 }
-	 
-	 public String getId() {
-	  return id;
-	 }
-	 public void setId(String id) {
-	  this.id = id;
-	 }
-	 public String getName() {
-	  return name;
-	 }
-	 public void setName(String name) {
-	  this.name = name;
-	 }
+	public static ArrayList<Object> IntersectAL(ArrayList<Object> al,HashMap<Object,Object> hm,Boolean inversion){
+		ArrayList<Object> value = new ArrayList<Object>();
+		for(int i=0;i<al.size();i++){
+			Object obj = al.get(i);
+			if(hm.containsKey(obj)&&!inversion){
+				value.add(hm.get(obj));
+			}else if(hm.containsKey(obj)&&inversion){
+				hm.remove(obj);
+			}
+		}
+		if(inversion){
+			Iterator<Object> it=hm.keySet().iterator();
+			while (it.hasNext()) {
+				Object inversionKey=it.next();
+				value.add(hm.get(inversionKey));
+			}
+		}
+		return value;
+	}
 	
+	public static HashMap<Object,Object> IntersectHM(ArrayList<Object> al,HashMap<Object,Object> hm){
+		HashMap<Object,Object> value = new HashMap<Object,Object>();
+		for(int i=0;i<al.size();i++){
+			Object obj = al.get(i);
+			if(hm.containsKey(obj))
+				value.put(obj, hm.get(obj));
+		}
+		return value;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Object> al=new  ArrayList<Object>();
+		al.add("1");
+		al.add("2");
+		al.add("3");
+		al.add("4");
+		
+		HashMap<Object,Object> hm = new HashMap<Object,Object>();
+		hm.put("4", "第四");
+		hm.put("7", "第七");
+		hm.put("6", "第六");
+		
+		ArrayList<Object> ao = IntersectAL(al, hm, true);
+		for(Object value:ao){
+			System.out.println(value);
+		}
+		
+	}
 }
+
+	
